0434fac6a1e13200726e2da27b1b07bad7fa7fed
Inana Susa <inana.programming@gmail.com>
2025-06-08 16:59:16 +0300

    Added feature for making requests

---
+++ src/features/pages/content-and-planner/IdeasVault.js
@@ -0,0 +1,13 @@
+import React from "react";
+import RequestCSV from "./RequestCSV";
+
+export default function IdeasVault () {
+
+    return (
+        <div className="ideas-vault">
+
+            <h3>Ideas Vault</h3>
+            <RequestCSV />
+        </div>
+    );
+}
