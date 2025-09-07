0025b46212a998a4bea25e5e343b3c56cadb437c
Inana Susa <inana.programming@gmail.com>
2025-09-07 13:53:39 +0300

    Styled welcome message (admin)

--- src/features/layouts/AdminDashboardLayout.js
+++ src/features/layouts/AdminDashboardLayout.js
@@ -3,9 +3,11 @@ import React, { useEffect } from "react";
 import Sidebar from "../components/Sidebar";
 import { Outlet } from "react-router-dom";
 import '../../style/desktop.css';
+import { useSelector } from "react-redux";
 
 export default function AdminDashboardLayout({username}) {
 
+  const user = useSelector(s => s.user.data);
 
   return (
     <div className="dashboard-container">
@@ -14,7 +16,7 @@ export default function AdminDashboardLayout({username}) {
         <Sidebar />
       </div>
       <div className="main-content">
-        <h2>Welcome, {username}!</h2>
+        <h2 className="welcome">Welcome, <span className="welcome-username" >{user.username}</span>!</h2>
         <Outlet />
       </div>
     </div>
