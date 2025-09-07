1e0a94ba01a75bdd9642bd8f19c5e1a29880898b
Inana Susa <inana.programming@gmail.com>
2025-09-07 11:33:21 +0300

    Styled change password page

--- src/features/pages/profile/ChangePassword.js
+++ src/features/pages/profile/ChangePassword.js
@@ -7,13 +7,13 @@ import "../../../style/dektop/change-password.css";
 
 export default function ChangePassword() {
 
-    const [ userId, setUserId ] = useState();
-    const [ currentPassword, setCurrentPassword ] = useState('');
-    const [ newPassword, setNewPassword ] = useState('');
+    const [userId, setUserId] = useState();
+    const [currentPassword, setCurrentPassword] = useState('');
+    const [newPassword, setNewPassword] = useState('');
 
     const navigate = useNavigate();
 
-   
+
     const handleSubmit = async (e) => {
         e.preventDefault();
         const token = localStorage.getItem('token');
@@ -25,10 +25,10 @@ export default function ChangePassword() {
         if (newPassword.length < 8) {
             alert("New password should be at least 8 characters long!");
         }
-        
+
 
         try {
-            const res = await axios.post('http://localhost:5000/me/change-password', {userId, currentPassword, newPassword}, {headers: {Authorization: `Bearer: ${token}`}});
+            const res = await axios.post('http://localhost:5000/me/change-password', { userId, currentPassword, newPassword }, { headers: { Authorization: `Bearer: ${token}` } });
             alert('Password changed successfuly!');
             setCurrentPassword('');
             setNewPassword('');
@@ -38,18 +38,21 @@ export default function ChangePassword() {
             console.log(err);
             alert('Failed to change password');
         }
-        
+
     }
     return (
-    <div className="change-password">
-        <h2>Change Password</h2>
+        <div className="change-password">
+            <h2>Change Password</h2>
 
-        <form className="form" onSubmit={handleSubmit}>
-            <input type="password" name="currentPassword" value={currentPassword} onChange={(e) => setCurrentPassword(e.target.value)}/>
-            <input type="password" name="newPassword" value={newPassword} onChange={(e) => setNewPassword(e.target.value)}/>
-            <button type="submit">Done</button>
-        </form>
+            <form className="form" onSubmit={handleSubmit}>
+                <div className="change-password-input-fields">
+                    <input type="password" name="currentPassword" value={currentPassword} onChange={(e) => setCurrentPassword(e.target.value)} placeholder="Current Password" />
+                    <input type="password" name="newPassword" value={newPassword} onChange={(e) => setNewPassword(e.target.value)} placeholder="New Password" />
+                </div>
 
-        
-    </div>);
+                <button type="submit">Done</button>
+            </form>
+
+
+        </div>);
 }
