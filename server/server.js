f841bf04c3a0a335af6631d8ef3df48e48acc59c
DragonRider970901 <inana.programming@gmail.com>
2026-03-04 12:10:22 +0200

    Deleted cors duplicate line

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 console.log("BOOT_OK: running server/server.js", __filename);
 const express = require('express');
 
-const cors = require('cors');
 const cors = require("cors");
 
 const corsOptions = {
