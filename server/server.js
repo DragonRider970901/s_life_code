d0f5a58ca0f4cc79c6e55e12cb0785c2b85c71e7
DragonRider970901 <inana.programming@gmail.com>
2026-03-11 18:35:50 +0200

    Added console.log lines to check env variables values

--- server/server.js
+++ server/server.js
@@ -90,7 +90,10 @@ app.use(bodyParser.json());
 app.use('/uploads/profile_pics', express.static('uploads/profile_pics'));
 //connect to database
 //const db = mysql.createConnection(process.env.DATABASE_URL);
-
+console.log("DB_HOST =", process.env.DB_HOST);
+console.log("DB_USER =", process.env.DB_USER);
+console.log("DB_NAME =", process.env.DB_NAME);
+console.log("DB_PORT =", process.env.DB_PORT);
 const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
