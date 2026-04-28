bcde34fe4772ac12b4732ac1f103afa44665b200
DragonRider970901 <inana.programming@gmail.com>
2026-04-28 12:07:15 +0300

    Attempt to style my profile submenu (phone) pt 5

--- src/Root.js
+++ src/Root.js
@@ -90,8 +90,8 @@ export default function Root() {
                     {loggedIn && (
                         <>
                             <div className="my-profile-overlay">
-                                <NavLink to='dashboard/overview' className="main-menu-link main-menu-profile">My Profile</NavLink>
-                                <h3 className="my-profile-header">MY PROFILE</h3>
+                                <div><NavLink to='dashboard/overview' className="main-menu-link main-menu-profile">My Profile</NavLink></div>
+                                <div><h3 className="my-profile-header">MY PROFILE</h3></div>
                             </div>
                             <div className={`my-profile-submenu`}>
                                 <h3 className="dashboard-menu-header">CREATOR TOOLS</h3>
