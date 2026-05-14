e09bf288ffaa4c4a4f9adfd6989566eb79f0d78f
DragonRider970901 <inana.programming@gmail.com>
2026-05-14 10:53:01 +0300

    Display result on full result page pt 4

--- src/features/pages/FullResult.js
+++ src/features/pages/FullResult.js
@@ -18,9 +18,9 @@ export default function FullResult() {
     //const utypecode = determineType(profile);
     const dispatch = useDispatch();
 
-    useEffect(() => {
+    /*useEffect(() => {
         dispatch(fetchTypes());
-    }, []);
+    }, []);*/
 
     /*useEffect(() => {
 
@@ -59,6 +59,7 @@ export default function FullResult() {
     }
 
     useEffect(() => {
+        dispatch(fetchTypes());
         fetchProfile();
         //setUtypecode(determineType(profile));
     }, []);
